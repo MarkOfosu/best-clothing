f3f56de5df0ad295a1b91139e63ebb31ca4d8cad
MarkOfosu <marko.twum@gmail.com>
2023-03-09 15:54:22 -0800

    added sign in component as well as firebase utils and brought in firebase libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,21 @@
 
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+
 
 const SignIn = () =>{
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+       const userDocRef = await createUserDocumentFromAuth(user)
+    }
+
     return (
-        <h1>Sign-In</h1>
+        <div>
+            <h1>Sign-In</h1>
+            <button onClick={logGoogleUser}>
+            Sign in with Google Popup
+            </button>
+        </div>
     )
 }
 export default SignIn;
